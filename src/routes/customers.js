b4e34329fc0292bdbf486c7e14f71c427d7f6b22
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-12-29 08:45:06 -0300

    put route

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.post('', require('../services/customers/create'));
 router.delete('/:id', require('../services/customers/delete'));
 router.get('/:id', require('../services/customers/listOne'));
-router.get('', require('../services/customers/list'))
+router.get('', require('../services/customers/list'));
+router.put('/:id', require('../services/customers/update'));
 
 module.exports = router;
