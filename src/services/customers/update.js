97b274245e1686e2153b5f8c352647827294e435
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-12-29 09:15:45 -0300

    update

--- src/services/customers/update.js
+++ src/services/customers/update.js
@@ -1,5 +1,27 @@
 const Customers = require('../../schemas/customers');
+const updatePhone = require('../phones/update').update;
 
 module.exports = async (req, res) => {
-    
+
+    if (!req.params.id)
+        return res.status(400).json({ message: 'need to send customer id' });
+
+    Customers.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    }).then(customer => {
+
+        if (customer) {
+            
+
+            if (req.body.phones && req.body.phones.length) 
+                updatePhone(req.body.phones, req.params.id)
+
+            return res.status(201).json({ message: 'customer updated' })
+        }
+
+    }).catch(err => {
+        return res.status(500).json({ message: err.message })
+    })
 }
--- src/services/phones/update.js
+++ src/services/phones/update.js
@@ -2,8 +2,31 @@ const Phones = require('../../schemas/phones');
 
 async function update(phones, customerId) {
 
+    if (!customerId)
+        return "need to send customerId"
 
-
+    if (phones.length) {
+        for (const phone of phones) {
+            if (phone.id) {
+                Phones.update({
+                    ddd: phone.ddd,
+                    phone: phone.phone,
+                    customerId: customerId
+                }, {
+                    where: {
+                        id: phone.id
+                    }
+                })
+            }
+            else {
+                Phones.create({
+                    ddd: phone.ddd,
+                    phone: phone.phone,
+                    customerId: customerId
+                })
+            }
+        }
+    }
 }
 
 module.exports = {
