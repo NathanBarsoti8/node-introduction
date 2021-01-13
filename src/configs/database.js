502e7e055c65f00da353ae03c10d7afb977d1175
Nathan Barsoti <nathanbarsoti@hotmail.com>
2021-01-13 10:23:23 -0300

    install mysql

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,11 +1,12 @@
 module.exports = {
     development: {
-        dialect: 'mssql',
-        host: 'localhost',
-        port: 6400,
-        username: 'sa',
-        password: 'Node_introduction',
-        database: 'NodeIntroDB',
+        dialect: 'mysql',
+        host: 'bxcznacn4bhhqnrr3i7n-mysql.services.clever-cloud.com',
+        username: 'ux3ap6twsxvzq0uj',
+        port: 3306,
+        password: 'oVAnQqgKm0MnNcWFQwLL',
+        database: 'bxcznacn4bhhqnrr3i7n',
+        logging: false,
         define: {
             freezeTableName: true,
             timestamps: true,
@@ -13,3 +14,17 @@ module.exports = {
         }
     }
 }
+
+// development: {
+//     dialect: 'mssql',
+//     host: 'localhost',
+//     port: 6400,
+//     username: 'sa',
+//     password: 'Node_introduction',
+//     database: 'NodeIntroDB',
+//     define: {
+//         freezeTableName: true,
+//         timestamps: true,
+//         undescored: false
+//     }
+// }
